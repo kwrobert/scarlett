a1d77d3cf4933aa42db073c3331f075c103a8d8d
Kyle Robertson <kyle.wesley@me.com>
2021-04-19 17:46:50 -0400

    Added automatic prompt generation from CSV to generate. Removed bug where prompts were generated from empty rows

--- gpt_2/generate.py
+++ gpt_2/generate.py
@@ -1,38 +1,70 @@
 import os
+import pathlib
+import argparse
+import csv
 from datetime import datetime
 import gpt_2_simple as gpt2
 
+SCRIPT_DIR = pathlib.Path(__file__).parent.absolute()
+DEFAULT_RESULTS_DIR = SCRIPT_DIR.joinpath("..", "data", "raw")
+
+
+def generate_prompts_from_csv(path):
+    path = pathlib.Path(path)
+    prompts = []
+    with open(path, "r", encoding='utf-8-sig') as f:
+        fieldnames = f.readline().lstrip("#").strip().split(",")
+        print("fieldnames = {}".format(fieldnames))
+        reader = csv.DictReader(f, fieldnames=fieldnames)
+        for row in reader:
+            if not any(v for v in row.values()):
+                continue
+            print("row = {}".format(row))
+            prompt = f"""
+[LABELS]
+
+title: {row['title']}
+
+store_name: {row['store_name']}
+
+store_location: {row['store_location']}
+
+page_template: {row['page_template']}
+
+keywords: {row['keywords']}
+"""
+            prompts.append((row["file_name"], prompt))
+    return prompts
+
 
 def main():
+    parser = argparse.ArgumentParser(description="""""")
+    parser.add_argument(
+        "-m",
+        "--model-name",
+        required=True,
+        help="""Name of finetuned model run to use for generation""",
+    )
+    parser.add_argument("-c", "--csv", help="""Path to CSV with prompt metadata""")
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        default=DEFAULT_RESULTS_DIR,
+        help="""Directory to save generated samples to""",
+    )
+    args = parser.parse_args()
 
-    pages_to_gen = [
-        # "Burlington Carpet One Floor & Home Custom Area Rugs",
-        # "Hosner Carpet One Floor & Home Custom Area Rugs",
-        # "Skawg Brothers Carpet One Floor & Home Kitchen and Bathroom Flooring",
-        # "I-Five Carpet One Floor & Home Kitchen and Bathroom Tile"
-        # "Types of Luxury Vinyl",
-        # "Waterproof Vinyl Page",
-        "Stain Resistant Carpet Page",
-        "Types of Vinyl Page",
-        "Commercial Flooring Page"
-        # "Stair Runner Carpet",
-        # "Types of Carpet",
-        # "Waterproof Luxury Vinyl",
-        # "Custom Area Rugs"
-        "Carpet Guide Page"
-    ]
-    
+    prompts = generate_prompts_from_csv(args.csv)
+
     # gen_file = "generated_samples/gpt2_gentext_{:%Y%m%d_%H%M%S}.txt".format(
     #     datetime.utcnow()
     # )
-    run_name = "first_test"
     sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name=run_name)
+    gpt2.load_gpt2(sess, run_name=args.model_name)
     # gpt2.generate(sess, run_name='first_test', prefix="Luxury Vinyl Plank Flooring")
-    for prefix in pages_to_gen:
-        fname = prefix.replace(" ", "_") + ".txt"
+    for fname, prefix in prompts:
         print(f"Saving samples for prefix {prefix} to {fname}")
-        gen_file = os.path.join("generated_samples", fname)
+        gen_file = os.path.join(args.output_dir, fname)
         gpt2.generate_to_file(
             sess,
             prefix=prefix,
@@ -41,7 +73,7 @@ def main():
             temperature=0.7,
             nsamples=5,
             batch_size=1,
-            run_name=run_name,
+            run_name=args.model_name,
         )
 
 
